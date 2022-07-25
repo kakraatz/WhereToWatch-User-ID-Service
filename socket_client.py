7ba9e5af6015115252c61e83b91c68e201219a60
Kevin Kraatz <86261021+kakraatz@users.noreply.github.com>
2022-07-25 14:30:48 -0500

    Add files via upload

--- socket_client.py
+++ socket_client.py
@@ -1,17 +1,20 @@
 # Source - https://realpython.com/python-sockets/
 
 import socket
+import json
 
 
-def microservice(login):
+def microservice(name, service):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
-        my_socket.connect(("127.0.0.1", 50000))
-        my_socket.sendall(bytes(login.encode()))
-        response = my_socket.recv(1024).decode()
+        my_socket.connect(("127.0.0.1", 5000))
+        info = json.dumps({"name": name, "service": service})
+        my_socket.sendall(bytes(info.encode()))
+        response = my_socket.recv(1024)
+        response = response.decode()
         user_data = response.strip("][").split(", ")
+    message = ("Login ID created for user: " + user_data[0] + ", Service: " + user_data[1] + ", ID: " + str(user_data[2]))
+    print(message)
+    return user_data
 
-    message = ("Login ID created for user: " + str(user_data[0]) + ", ID: " + str(user_data[1]))
-    return message
 
-
-microservice("testuser")
+microservice("testuser", "Netflix")
